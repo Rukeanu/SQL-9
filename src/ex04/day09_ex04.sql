9b43a61920e6456fe91c65045413ace9e09c6604
Rukeanu <sansastarkqueenofnorth@gmail.com>
2024-08-27 14:59:18 +0300

    day-09

---
+++ src/ex04/day09_ex04.sql
@@ -0,0 +1,33 @@
+CREATE FUNCTION fnc_persons_male()
+    RETURNS TABLE
+            (
+                id      person.id%TYPE,
+                name    person.name%TYPE,
+                age     person.age%TYPE,
+                gender  person.gender%TYPE,
+                address person.address%TYPE
+            )
+    LANGUAGE SQL
+AS
+$BODY$
+SELECT *
+FROM person
+WHERE gender = 'male';
+$BODY$;
+
+CREATE FUNCTION fnc_persons_female()
+    RETURNS TABLE
+            (
+                id      person.id%TYPE,
+                name    person.name%TYPE,
+                age     person.age%TYPE,
+                gender  person.gender%TYPE,
+                address person.address%TYPE
+            )
+    LANGUAGE SQL
+AS
+$BODY$
+SELECT *
+FROM person
+WHERE gender = 'female';
+$BODY$;
